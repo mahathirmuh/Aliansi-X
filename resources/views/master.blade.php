34ec23ff9aa7687ca9795cbfd9d02a78013691c1
Mahathir Muhammad <Mahathirmuhammad02@gmail.com>
2018-08-16 02:50:24 +0800

    3x fissabillah
    3x fissabillah

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,64 +1,384 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Beranda Desa</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
 
-        <link rel="stylesheet" href="{{ asset('asset/css/bootstrap.min.css') }}">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Desa Mendik</title>
+  <!-- Favicons -->
+  <link rel="icon" href="{{asset('img/favicon-16x16.png')}}">
+  <link href="{{asset('img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-        <link rel="icon" href="{{asset('img/favicon-16x16.png')}}">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
 
-        <!-- Styles -->
-    </head>
-    <body>
-        <div class="header">
-          <div class="col-xs-12">
-            <div class="col-xs-2 col-lg-2">
-              <a href="http://www.paserkab.go.id/home/web"><img src="{{ asset('img/daya-taka-colour_burned.png') }}" alt="logo desa" id="logo_desa"></a>
-            </div>
-            <div class="col-xs-8 col-lg-8">
-              <a href="{{url('/')}}"> <p>Selamat Datang di Website <br>Desa Mendik <br>Agamis, Mandiri, Berbudaya dan Sejahtera</p> </a>
-            </div>
-            <div class="col-xs-2 col-lg-2">
-              <a href="http://www.unmul.ac.id/"><img src="{{ asset('img/Logo-Unmul-2.png') }}" alt="logo_unmul" id="logo_unmul"></a>
+  <!-- Bootstrap CSS File -->
+  <link href="{{asset('lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="{{asset('lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+  <link href="{{asset('lib/animate/animate.min.css')}}" rel="stylesheet">
+  <link href="{{asset('lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+  <link href="{{asset('lib/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
+  <link href="{{asset('lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="{{asset('css/main.css')}}" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: Reveal
+    Theme URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body id="body">
+
+  <!--==========================
+    Top Bar
+  ============================-->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container clearfix">
+      <div class="contact-info float-left">
+        <i class="fa fa-envelope-o"></i> <a href="mailto:Mahathirmuhammad02@gmail.com">MahathirMuhammad02@gmail.com</a>
+        <i class="fa fa-phone"></i> +62 853 4865 2074
+      </div>
+      <div class="social-links float-right">
+        <a href="https://www.twitter.com/mauliahar" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="https://wwww.facebook.com/100008526373400" class="facebook"><i class="fa fa-facebook"></i></a>
+        <a href="https://wwww.instagram.com/mahathirmuh" class="instagram"><i class="fa fa-instagram"></i></a>
+        <!-- <a href="" class="google-plus"><i class="fa fa-google-plus"></i></a> -->
+        <!-- <a href="" class="linkedin"><i class="fa fa-linkedin"></i></a> -->
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#body" class="scrollto">KONOHA<span>TSUNA</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <!-- <li class="menu-has-childre"><a href="#">Beranda</a>
+            <ul>
+              <li><a href="#">Profil & Sejarah</a></li>
+              <li><a href="#">Visi & Misi</a></li>
+            </ul> -->
+          <!-- </li> -->
+          <!-- <li <a href="{{url('/')}}">Beranda</a></li> -->
+          <li><a href="{{url('/')}}">Beranda</a></li>
+          <li><a href="{{url('/kegiatan-desa')}}">Kegiatan Desa</a></li>
+          <li><a href="{{url('/struktur-organisasi')}}">Struktur Organisasi</a></li>
+          <!-- <li><a href="#portfolio">Portfolio</a></li> -->
+          <!-- <li><a href="#team">Peta Desa</a></li> -->
+          <li><a style="color:#50d8af; "href="#contact"><i class="fa fa-phone"></i> Hubungi Kami</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro">
+
+    <div class="intro-content">
+      <h2>SELAMAT<span>DATANG</span><br>DI WEBSITE RESMI DESA X</h2>
+      <div>
+        <a href="#profil" class="btn-get-started scrollto">Profil & Sejarah</a>
+        <a href="#peta" class="btn-projects scrollto">Peta Desa</a>
+      </div>
+    </div>
+
+    <div id="intro-carousel" class="owl-carousel" >
+      <div class="item"> <img src="{{url('img/inicover/a.jpg ')}}" alt=""> </div>
+      <div class="item"> <img src="{{url('img/inicover/a.jpg ')}}" alt=""> </div>
+      <div class="item"> <img src="{{url('img/inicover/a.jpg ')}}" alt=""> </div>
+      <div class="item"> <img src="{{url('img/inicover/a.jpg ')}}" alt=""> </div>
+      <div class="item"> <img src="{{url('img/inicover/a.jpg ')}}" alt=""> </div>
+    </div>
+
+  </section><!-- #intro -->
+
+  <main id="main">
+
+    <!--==========================
+      Peta Section
+    ============================-->
+
+
+    <!--==========================
+      Services Section
+    ============================-->
+    <section id="profil">
+      <div class="container">
+        <div class="section-header">
+          <h2>PROFIL & SEJARAH</h2>
+          <p>Sed tamen tempor magna labore dolore dolor sint tempor duis magna elit veniam aliqua esse amet veniam enim export quid quid veniam aliqua eram noster malis  adawdaw dasdiahsdoa asfoafbaoufaw qw oiaosifnawoif awfbawofwaiofbaw afoaiwbfwoiafbaw awbfoawfbwaoifa Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ulla duis fugiat culpa esse aute nulla ipsum velit export irure minim illum fore</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+            <div class="box wow fadeInLeft">
+              <div class="icon"><i class="fa fa-bar-chart"></i></div>
+              <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident etiro rabeta lingo.</p>
             </div>
           </div>
-        </div>
-        <!-- End of Header -->
-        <div class="navigation">
-          <nav class="navbar navbar-blue">
-            <div class="container-fluid">
-                <ul class="nav navbar-nav navbar-center">
-                  <li><a href="{{ url('/') }}">Home</a></li>
-                  <li><a href="{{ url('/kegiatan-desa') }}">Kegiatan Desa</a></li>
-                  <li><a href="{{url('/struktur-organisasi')}}">Struktur Organisasi Desa</a></li>
-                  <li><a href="#">Peta Desa Mendik</a></li>
-                </ul>
+
+          <div class="col-lg-6">
+            <div class="box wow fadeInRight">
+              <div class="icon"><i class="fa fa-picture-o"></i></div>
+              <h4 class="title"><a href="">Dolor Sitema</a></h4>
+              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
             </div>
-          </nav>
-        </div>
-        <!-- <End of Navbar> -->
-
-        <section class="content">
-            @yield('content')
-            <!-- End of Content -->
-        </section>
-
-        <footer>
-          <div class="copyright">
-            <b>
-              © 2018 Copyright: <a href="https://www.instagram.com/kkn44_desamendik/"> KKN 44 UNMUL Desa Mendik</a>
-              Design/Code by <a href="https://www.facebook.com/widyatamathebluedemon">TamaGotchii</a>
-            </b>
           </div>
-        </footer>
-        <!-- End of Footer -->
-        <script src="{{ asset('asset/js/jquery.js') }}"></script>
-        <script src="{{ asset('asset/js/bootstrap.min.js') }}" charset="utf-8"></script>
-    </body>
+
+          <div class="col-lg-6">
+            <div class="box wow fadeInLeft" data-wow-delay="0.2s">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
+              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur trinige zareta lobur trade.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box wow fadeInRight" data-wow-delay="0.2s">
+              <div class="icon"><i class="fa fa-map"></i></div>
+              <h4 class="title"><a href="">Magni Dolores</a></h4>
+              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum rideta zanox satirente madera</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #services -->
+
+    <section id="peta" class="wow fadeInUp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 about-img">
+            <img src="img/about-img.jpg" alt="">
+          </div>
+
+          <div class="col-lg-6 content">
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
+            <h3>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+
+            <ul>
+              <li><i class="ion-android-checkmark-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+              <li><i class="ion-android-checkmark-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+              <li><i class="ion-android-checkmark-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #Peta -->
+    <!--==========================
+      Our Portfolio Section
+    ============================-->
+    <section id="portfolio" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+          <h2>Galeri Kegiatan Desa</h2>
+          <p>Sed tamen tempor magna labore dolore dolor sint tempor duis magna elit veniam aliqua esse amet veniam enim export quid quid veniam aliqua eram noster malis nulla duis fugiat culpa esse aute nulla ipsum velit export irure minim illum fore</p>
+        </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="row no-gutters">
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/portfolio/1.jpg" class="portfolio-popup">
+                <img src="img/portfolio/1.jpg" alt="">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp">Portfolio Item 1</h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/portfolio/2.jpg" class="portfolio-popup">
+                <img src="img/portfolio/2.jpg" alt="">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp">Portfolio Item 2</h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/portfolio/3.jpg" class="portfolio-popup">
+                <img src="img/portfolio/3.jpg" alt="">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp">Portfolio Item 3</h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/portfolio/4.jpg" class="portfolio-popup">
+                <img src="img/portfolio/4.jpg" alt="">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp">Portfolio Item 4</h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/portfolio/5.jpg" class="portfolio-popup">
+                <img src="img/portfolio/5.jpg" alt="">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp">Portfolio Item 5</h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/portfolio/6.jpg" class="portfolio-popup">
+                <img src="img/portfolio/6.jpg" alt="">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp">Portfolio Item 6</h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/portfolio/7.jpg" class="portfolio-popup">
+                <img src="img/portfolio/7.jpg" alt="">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp">Portfolio Item 7</h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/portfolio/8.jpg" class="portfolio-popup">
+                <img src="img/portfolio/8.jpg" alt="">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp">Portfolio Item 8</h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #portfolio -->
+
+    <!--==========================
+      Contact Section
+    ============================-->
+    <section id="contact" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+          <h2>Hubungi Kami</h2>
+          <p>Untuk Informasi Lebih Lanjut Silahkan Menghubungi Pada Kontak Dibawah Ini :</p>
+        </div>
+
+        <div class="row contact-info">
+
+          <div class="col-md-4">
+            <div class="contact-address">
+              <i class="ion-ios-location-outline"></i>
+              <h3>Address</h3>
+              <address>A108 Adam Street, NY 535022, USA</address>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-phone">
+              <i class="ion-ios-telephone-outline"></i>
+              <h3>Phone Number</h3>
+              <p><a href="tel:+155895548855">+1 5589 55488 55</a></p>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-email">
+              <i class="ion-ios-email-outline"></i>
+              <h3>Email</h3>
+              <p><a href="mailto:Mahathirmuhammad02@gmail.com">info@example.com</a></p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <!-- <div id="google-map" data-latitude="40.713732" data-longitude="-74.0092704"></div> -->
+    </section>
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Aliansi Suna-Konoha</strong>. KKN 44 UNMUL
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Reveal
+        -->
+        Designed by <a href="https://www.facebook.com">Aliansi</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="{{asset('lib/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('lib/jquery/jquery-migrate.min.js')}}"></script>
+  <script src="{{asset('lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('lib/easing/easing.min.js')}}"></script>
+  <script src="{{asset('lib/superfish/hoverIntent.js')}}"></script>
+  <script src="{{asset('lib/superfish/superfish.min.js')}}"></script>
+  <script src="{{asset('lib/wow/wow.min.js')}}"></script>
+  <script src="{{asset('lib/owlcarousel/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('lib/magnific-popup/magnific-popup.min.js')}}"></script>
+  <script src="{{asset('lib/sticky/sticky.js')}}"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script>
+  <!-- Contact Form JavaScript File -->
+  <!-- <script src="('contactform/contactform.js')}}"></script> -->
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
 </html>
